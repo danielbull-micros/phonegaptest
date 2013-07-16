7ef9d79be8004037d57c8585024901406167ce44
danielbull-micros <danbull@micros.com>
2013-07-16 13:53:29 +0100

    Stuff

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
 var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-
    scanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      }, 
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
-
-
+  function (result) {
+  alert("We got a barcode\n" +
+  "Result: " + result.text + "\n" +
+  "Format: " + result.format + "\n" +
+  "Cancelled: " + result.cancelled);
+  }, 
+  function (error) {
+  alert("Scanning failed: " + error);
+  }
+);
 
 $(function() {
      
 	$('#qrTest').on('click', function () {
-		scanner.scan();
+		console.log("CLICK");
 	});
 
  });
